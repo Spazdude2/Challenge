d6d93eca7d30d8f82efa3bc4ef2f031b33db0db8
Spazdude2 <spaziani96@gmail.com>
2017-10-25 21:52:19 -0400

    Map Rotation

---
+++ Global Gravity/Assets/GobalGravity.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GobalGravity : MonoBehaviour 
+{
+	public CharacterController Map;
+
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		Map = GetComponent<CharacterController> ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+		if (Input.GetKeyDown (KeyCode.UpArrow)) 
+		{
+			Map.transform.Rotate (90, 0, 0);
+		}
+
+		if (Input.GetKeyDown (KeyCode.DownArrow)) 
+		{
+			Map.transform.Rotate (-90, 0, 0);
+		}
+
+		if (Input.GetKeyDown (KeyCode.RightArrow)) 
+		{
+			Map.transform.Rotate (0, 0, 90);
+		}
+
+		if (Input.GetKeyDown (KeyCode.LeftArrow)) 
+		{
+			Map.transform.Rotate (0, 0, -90);
+		} 
+			
+	}
+}
---
+++ Global Gravity/Assets/Jump.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour 
+{
+	public float JumpPower;
+	float SetJumpPower;
+	public int jumps;
+	int SetJumps;
+
+	Rigidbody rb;
+
+	GameObject Player;
+	// Use this for initialization
+	void Start () 
+	{
+		Player = this.transform.gameObject;
+		rb = this.GetComponent<Rigidbody> ();
+		SetJumps = jumps;
+		SetJumpPower = JumpPower;
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		if (jumps > 0) 
+		{
+			if (Input.GetButtonDown ("Jump")) 
+			{
+				jumps--;
+				rb.GetComponent<Rigidbody> ().AddForce (0, JumpPower, 0);
+			}
+		}
+	}
+
+	void OnCollisionEnter (Collision collision)
+	{
+		jumps = SetJumps;
+		JumpPower = SetJumpPower;
+	}
+}
